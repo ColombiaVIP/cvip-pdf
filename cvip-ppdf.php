358bfb9de82f51441ad976b03c1561c73172f0c5
ColombiaVIP <ingeniero.leon@colombiavip.com>
2024-12-30 15:03:10 -0500

    20241230

--- cvip-ppdf.php
+++ cvip-ppdf.php
@@ -18,16 +18,35 @@ function cvipppdf_add_pdf_link() {
             global $product;
             $featuredURL = get_the_post_thumbnail_url();
 
-            $attachment_ids = $product->get_gallery_image_ids();
+            // Obtener el logo del sitio
+            $logo = get_theme_mod( 'custom_logo' );
+            $logo =  wp_get_attachment_url( $logo );
+
+            // Obtener el nombre del sitio
+            $siteName = get_bloginfo('name');
+            $textoPie= "www.YourSite.com | phone: 506 8888-8888 | Columbia Central city";
+
             //Guardar la descripcion del producto
             $descripcion = $product->get_description();
             //escapa la descipcion
-            $descripcion = wp_strip_all_tags(json_encode($descripcion));
+            $descripcion = 
+            substr(str_replace("'","",
+                wp_strip_all_tags(
+                    json_encode($descripcion)
+                )
+            ), 0,2000);
             $anchoFeatured = 100;
             $anchoGallery = 50;
             $posGalleryX = 5;
             $posGalleryY = 5+$anchoFeatured;
             $image_urls = [];
+
+
+
+            $attachment_ids = $product->get_gallery_image_ids();
+            //Obtengo solo los primeros 6 elementos
+            $attachment_ids = array_slice($attachment_ids, 0, 6);
+
             foreach( $attachment_ids as $attachment_id ) {
                 $image_urls[] = wp_get_attachment_url( $attachment_id );
             }
@@ -38,6 +57,7 @@ function cvipppdf_add_pdf_link() {
                 const { jsPDF } = window.jspdf;
 
                 const doc = new jsPDF();
+
 
                 //FEATURED
                 doc.addImage(
@@ -70,7 +90,41 @@ function cvipppdf_add_pdf_link() {
                         $count++;
                     }
                 }  ?>
-                doc.text('<?=$descripcion ?>', 110, 5);
+                doc.setFontSize(10);
+                doc.text(
+                    '<?=$descripcion ?>', 
+                    110, 
+                    10,
+                    { maxWidth: 90 }
+                );
+                doc.addImage(
+                    '<?=$logo?>', 
+                    'JPG', 
+                    5, 
+                    260, 
+                    30, 
+                    30                    
+                );
+                doc.text(
+                    '<?=$textoPie ?>', 
+                    60, 
+                    280,
+                    { maxWidth: 200 }
+                );
+
+                doc.setPage(1);
+                doc.saveGraphicsState();
+                doc.setGState(new doc.GState({opacity: 0.1}));
+                //LOGO
+                doc.addImage(
+                    '<?=$logo?>', 
+                    'JPG', 
+                    5, 
+                    5, 
+                    200, 
+                    200                    
+                );
+                window.open(doc.output('bloburl'))
                 doc.save('output.pdf');
                 
 
