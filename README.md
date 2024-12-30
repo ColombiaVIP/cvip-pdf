1fd048eab11bb053ef70376bab049cdfae38b92e
ColombiaVIP <ingeniero.leon@colombiavip.com>
2024-12-30 13:12:25 -0500

    20241230

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # cvip-ppdf
 Allows printing a WooCommerce product as PDF
+
+## Description
+This plugin retrieves the featured image of a post and generates a PDF document containing that image.
+
+## Installation
+1. Upload the plugin files to the `/wp-content/plugins/my-wordpress-plugin` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+
+## Usage
+Once activated, the plugin will automatically retrieve the featured image of the current post and provide an option to generate a PDF containing that image.
+
+## Features
+- Retrieves the featured image of a post.
+- Generates a PDF document with the featured image.
+
+## Localization
+The plugin is ready for translation. The language files can be found in the `languages` directory.
+
+## CSS Styles
+Custom styles for the plugin can be found in the `assets/css/style.css` file.
+
+## Support
+For support, please open an issue on the plugin's repository.
---
+++ cvip-ppdf.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Plugin Name: CVIP PRINT PDF
+ * Description: A plugin to generate a PDF of a product image, gallery and description.
+ * Version: 1.0
+ * Author: Your Name
+ */
+
+// Prevent direct access
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+function cvipppdf_add_pdf_link() {
+    if ( is_product() ) {
+
+        if (isset($_GET['generate-pdf'])) {
+            global $product;
+            $featuredURL = get_the_post_thumbnail_url();
+
+            $attachment_ids = $product->get_gallery_image_ids();
+            //Guardar la descripcion del producto
+            $descripcion = $product->get_short_description();
+            $anchoFeatured = 100;
+            $anchoGallery = 50;
+            $posGalleryX = 5;
+            $posGalleryY = 5+$anchoFeatured;
+            $image_urls = [];
+            foreach( $attachment_ids as $attachment_id ) {
+                $image_urls[] = wp_get_attachment_url( $attachment_id );
+            }
+            echo "Generando PDF";        
+            ?>
+            <script src="<?php echo plugin_dir_url( __FILE__ )?>/js/node_modules/jspdf/dist/jspdf.umd.min.js"></script>
+            <script>
+                const { jsPDF } = window.jspdf;
+
+                const doc = new jsPDF();
+
+                //FEATURED
+                doc.addImage(
+                    '<?=$featuredURL?>', 
+                    'JPEG', 
+                    5, 
+                    5, 
+                    <?=$anchoFeatured?>, 
+                    <?=$anchoFeatured?>
+                );
+                //GALLERY
+                <?php 
+                $count = 1;
+                foreach($image_urls as $imageURL) { ?>
+                    doc.addImage(
+                        '<?=$imageURL?>', 
+                        'JPEG', 
+                        <?=$posGalleryX?>,
+                        <?=$posGalleryY?>,
+                        <?=$anchoGallery?>,
+                        <?=$anchoGallery?>
+                    );
+                    <?php 
+                    $posGalleryX=($posGalleryX==5)?55:5; 
+                    if ($count == 2) { 
+                        $posGalleryY+=$anchoGallery;
+                        $count = 1; 
+                    }
+                    else {
+                        $count++;
+                    }
+                }  ?>
+                doc.text('<?=$descripcion ?>', 110, 5);
+                doc.save('output.pdf');
+                
+
+
+            </script>
+            <?php
+        }
+        else {
+            echo '<a href="?generate-pdf" id="generate-pdf-link">Generate PDF</a>';
+        }
+        
+    }
+}
+add_action( 'woocommerce_before_single_product', 'cvipppdf_add_pdf_link' );
+
+?>
